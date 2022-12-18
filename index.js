0d8b0526d20a20556c1bc40eb93c8c5431107af7
muhammadAmeen786 <mohdameeendar@gmail.com>
2022-12-18 17:50:04 +0530

    added files

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-let box1 = document.querySelector('#box-1');
-let box2 = document.querySelector('.box-2');
-document.querySelector('button').addEventListener('click',()=>{
-   box2.innerHTML = box1.value
-});
+var inputText = document.querySelector("#input-text")
+var translateBtn = document.querySelector("#translate-btn")
+var outputText = document.querySelector("#output-text")
+var errorMsg = document.querySelector(".error-msg")
 
+// var serverurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverurl ="https://api.funtranslations.com/translate/minion.json"
+
+function  urlAdd(inputurl){
+    return serverurl +"?"+"text="+ inputurl
+}
+
+function textdisplaying(){
+    var displayingMsg = inputText.value;
+ 
+    fetch(urlAdd(displayingMsg))
+    .then(response => response.json())
+    .then(json => {
+        var translateText = json.contents.translated;
+        outputText.innerText = translateText;
+        console.log(translateText)
+        outputText.scrollIntoView()
+    })
+    .catch(() => alert("some error occured"))
+}
+
+translateBtn.addEventListener("click",function(){
+   
+    if( inputText.value === "" ){
+        outputText.innerText = ""
+        errorMsg.style.display = "block"
+        outputText.style.border = "none"
+        errorMsg.innerText = "please type text what you want to translate"
+       
+    }else{
+        errorMsg.style.display = "none"
+        inputText.style.color  = "rgb(100, 100, 100)"
+        outputText.style.border = "2px solid rgba(34, 34, 194, 0.815)"
+        textdisplaying()
+    }
+
+})
+
+console.log(urlAdd)
